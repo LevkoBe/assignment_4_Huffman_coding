ddb835b2594fef8bf0667fdf8f77a78f694fdd57
Levko <lbeniakh@kse.org.ua>
2023-03-31 10:50:04 +0300

    Starting commit with reading "Adventures..."

---
+++ assignment 4 - Huffman coding/Program.cs
@@ -0,0 +1,19 @@
+﻿var allCharacters = new Dictionary<char, int>();
+foreach (var line in File.ReadAllLines(
+             "D:\\C#Projects\\assignment 4 - Huffman coding\\assignment 4 - Huffman coding\\THE ADVENTURE OF THE DANCING MEN.txt"))
+{
+    foreach (var character in line)
+    {
+        if (!allCharacters.ContainsKey(character))
+        {
+            allCharacters[character] = 0;
+        }
+
+        allCharacters[character]++;
+    }
+}
+
+foreach (var character in allCharacters)
+{
+    Console.WriteLine($"Character {character.Key} has {character.Value} implementations.");
+}
